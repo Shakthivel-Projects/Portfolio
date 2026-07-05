8e718c65df5aaf091b251a7a0deb71be967b0d5b
Shakthivel <shakthivel1215@gmail.com>
2026-07-05 17:39:19 +0530

    new Feature added - Sticky Navbar

--- script.js
+++ script.js
@@ -14,3 +14,46 @@ setInterval(() => {
     if(index == roles.length)
         index = 0;
 }, 2000);
+
+
+const hamburger =
+    document.getElementById("hamburger");
+
+const navItems =
+    document.getElementById("navItems");
+
+hamburger.addEventListener("click",()=>{
+
+    navItems.classList.toggle("active");
+
+});
+
+document
+.querySelectorAll(".nav-items a")
+.forEach(link=>{
+
+    link.addEventListener("click",()=>{
+
+        navItems.classList.remove(
+            "active"
+        );
+
+    });
+
+});
+
+const navbar = 
+    document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+    if(window.screenY > 50){
+        navbar.classList.add(
+            "sticky"
+        );
+    }
+    else{
+        navbar.classList.remove(
+            "sticky"
+        );
+    }
+});
